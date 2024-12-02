6efb9bf1db283d91df95f59612f4aec736d8be4f
Emily <emilia.lopezf.1999@gmail.com>
2024-12-02 19:41:23 -0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,14 @@ project.group = "io.github.emilyy-dev"
 project.version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(8)
+  }
 }
 
 license {
     header = file("header.txt")
-    encoding = Charsets.UTF_8.name()
+    encoding = "UTF-8"
     mapping("java", "DOUBLESLASH_STYLE")
     include("**/*.java")
 }
@@ -22,22 +23,22 @@ repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.extendedclip.com/releases/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc", "spigot-api", "1.8.8-R0.1-SNAPSHOT") {
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT") {
         exclude("net.md-5", "bungeecord-chat")
     }
-    compileOnly("io.github.emilyy-dev", "betterjails-api", "1.5-SNAPSHOT")
-    compileOnly("me.clip", "placeholderapi", "2.10.10")
+    compileOnly("io.github.emilyy-dev:betterjails-api:1.5")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 
-    compileOnly("org.jetbrains", "annotations", "21.0.1")
+    compileOnly("org.jetbrains:annotations:26.0.1")
 }
 
 tasks {
     check {
-        finalizedBy(licenseMain)
+        dependsOn(licenseMain)
     }
 
     compileJava {
